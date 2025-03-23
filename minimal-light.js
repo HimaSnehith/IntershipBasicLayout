cb46ef32842d4415c1c976b48e3d03f29478dbac
HimaSnehith <129297196+HimaSnehith@users.noreply.github.com>
2025-03-23 04:01:28 -0700

    Add files via upload
    Set-2

---
+++ minimal-light.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".status").addEventListener("click", function() {
+        alert("Patient status is being reviewed.");
+    });
+});
